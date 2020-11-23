b67e7bebff9417803a594608dabc1445ee256c3b
Matt-1985 <blatsche@web.de>
2020-11-23 17:25:03 +0100

    Serve client and storybook

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.get("/", (request, response) => {
-  response.send("Hello World!");
+app.use(express.static(path.join(__dirname, "client/build")));
+app.use(
+  "/storybook",
+  express.static(path.join(__dirname, "client/storybook-static"))
+);
+
+// Handle React routing, return all requests to React app
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/build", "index.html"));
 });
 
 app.listen(port, () => {
